ee1e60ec3dedb6778e86a951be2c2367eedae467
FernandoRAM <frincon20@alumnos.uaq.mx>
2019-03-23 00:18:52 -0600

    js profesores login

--- js/profesores.js
+++ js/profesores.js
@@ -1,4 +1,19 @@
 function iniciarSesion(){
     var correo =  document.getElementById('usuario');
     var pass = document.getElementById('password');
+
+    var loginXHR = new XMLHttpRequest();
+    loginXHR.open('POST', 'http://127.0.0.1:81/Hackathon2019/Profesores/login');
+    loginXHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    loginXHR.send('mail='+ 'fer@mail.com' +'&pass='+'12345');
+    loginXHR.onreadystatechange = function(){
+        if(loginXHR.status == 200 && loginXHR.readyState == 4){
+            console.log(JSON.parse(loginXHR.responseText));
+            if (loginXHR.responseText != '0'){
+                alert('Exito, '+ JSON.parse(loginXHR.responseText).Nombre);
+            }
+
+        }
+       
+    }
 }
