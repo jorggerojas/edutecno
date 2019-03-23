896dd0ec8dc4d96add0dd8d430ea9b8d9fac0951
FernandoRAM <frincon20@alumnos.uaq.mx>
2019-03-23 02:24:15 -0600

    get grupos

--- js/profesores.js
+++ js/profesores.js
@@ -43,4 +43,24 @@ function getGrupos(){
     gruposXHR.open('GET', 'http://127.0.0.1:81/Hackathon2019/Cursos/getGrupos?idProfesor=' + idP);
     gruposXHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     gruposXHR.send();
+    gruposXHR.onreadystatechange = function(){
+        if(gruposXHR.readyState == 4 && gruposXHR.status == 200){
+            loclas
+            //exito
+        }
+    }
+}
+
+function getMaterias(){
+    var idP = localStorage.getItem('idProfesor');
+    var idM = localStorage.getItem('idMateria');
+    materiasXHR = new XMLHttpRequest();
+    materiasXHR.open('GET', 'http://127.0.0.1:81/Hackathon2019/Cursos/getMaterias?idProfesor='+idP+'&idGrupo='+idM);
+    materiasXHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    materiasXHR.send();
+    materiasXHR.onreadystatechange = function(){
+        if(materiasXHR.readyState == 4 && materiasXHR.status == 200){
+            //exito 
+        }
+    }
 }
